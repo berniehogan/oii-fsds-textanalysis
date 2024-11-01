4d0bd07afc3de632f044790076feaeee0c738a2b
Viktoria <leinsviktoria@gmail.com>
2024-11-01 12:32:56 +0000

    Work on lab

--- models/reddit_scraper.py
+++ models/reddit_scraper.py
@@ -7,32 +7,35 @@ from datetime import datetime, timedelta
 
 def cache_results(func):
     def wrapper(self, subreddit, limit=100, cache=False, cache_duration_hours=24):
+        '''
+        Creates a path for the cache file .
+        '''
         cache_dir = '.cache'
         cache_file = os.path.join(cache_dir, f'{subreddit}_{limit}.json')
         
         if cache:
-            os.makedirs(cache_dir, exist_ok=True)
-            if os.path.exists(cache_file):
-                modified_time = datetime.fromtimestamp(os.path.getmtime(cache_file))
-                if datetime.now() - modified_time < timedelta(hours=cache_duration_hours):
-                    with open(cache_file, 'r') as f:
-                        return json.load(f)
+            os.makedirs(cache_dir, exist_ok=True) # creates a "cache" directory if it doesn't exist
+            if os.path.exists(cache_file): # checks if the cache file exists
+                modified_time = datetime.fromtimestamp(os.path.getmtime(cache_file)) # gets the last modified time of the cache file
+                if datetime.now() - modified_time < timedelta(hours=cache_duration_hours): # check if the cached results are still valid based on a specified duration
+                    with open(cache_file, 'r') as f: # reads the cache file
+                        return json.load(f) # returns the cached results as json
         
-        results = func(self, subreddit, limit)
+        results = func(self, subreddit, limit) # if the cache is not valid or doesn't exist, the function is called to get the results
         
         if cache:
-            with open(cache_file, 'w') as f:
-                json.dump(results, f)
+            with open(cache_file, 'w') as f: # writes the results to the cache file
+                json.dump(results, f) # writes the results as json
         
         return results
     return wrapper
 
-class RedditScraper:
+class RedditScraper: # same as Day2
     def __init__(self, user_agent):
         self.headers = {'User-Agent': user_agent}
         self.base_url = "https://api.reddit.com"
     
-    @cache_results
+    @cache_results # indicates that the results of this method should be cached
     def get_subreddit_posts(self, subreddit, limit=100, cache=False, cache_duration_hours=24):
         posts = []
         after = None
